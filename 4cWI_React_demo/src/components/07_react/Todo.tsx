8b7a310d480b6a438ee78aa0ab3a5999ccc70f39
beyzauenal <beyza.uenal@student.htldornbirn.at>
2025-12-08 22:06:38 +0100

    7 UseEffect

--- 4cWI_React_demo/src/components/07_react/Todo.tsx
+++ 4cWI_React_demo/src/components/07_react/Todo.tsx
@@ -13,3 +13,4 @@ export default function Todo({ title, completed }: Props) {
         </div>
     );
 }
+
--- 4cWI_React_demo/src/main.tsx
+++ 4cWI_React_demo/src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 //05_react
 //import App from "./components/05_react/App.tsx";
 //06_react
-import App from "./components/06_react/App.tsx";
+//import App from "./components/06_react/App.tsx";
 //07_react
-//import App from "./components/07_react/App.tsx";
+import App from "./components/07_react/App.tsx";
 
 import "./index.css";
 
